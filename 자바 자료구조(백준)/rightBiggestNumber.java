dacbeb408589b486267bf69d2f7e557405401762
bracksnow <ktom97@gmail.com>
2019-09-17 00:11:11 +0900

    no message

--- 자바 자료구조(백준)/rightBiggestNumber.java
+++ 자바 자료구조(백준)/rightBiggestNumber.java
@@ -1,6 +1,5 @@
 package DataStructure;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,38 +17,48 @@ public class rightBiggestNumber {
         StringTokenizer st = new StringTokenizer(bf.readLine());
         Stack stack = new Stack();
         int[] result = new int[num];
-        Arrays.fill(result,-1);
+        Arrays.fill(result, -1);
         int[] a = new int[num];
         int i;
-        for(i = 0;i<num;i++){
+        for (i = 0; i < num; i++) {
             a[i] = Integer.parseInt(st.nextToken());
-        }
-        for(i= 0; i<num; i++){
-            int j = i;
-            if(stack.empty()==true){
+        }//배열안에 있는 것을 숫자로 변환한다
+        for (i = 0; i < num; i++) {
+            if (stack.empty() == true) {
                 stack.push(a[i]);
-            }else{
-                while(stack.empty()!=true && (int)stack.peek()<a[i]){
-                    stack.pop();
-                    stack.push(a[i]);
-                    result[j-1] = a[i];
+                //숫자가 들어갔을 때부터 비교를 해줘야한다(고치기)
+            }
+            else {
+                while (stack.empty() != true) {
+                    if ((int) stack.peek() < a[i]) {
+                        stack.pop();
+                        stack.push(a[i]);
+                        System.out.println(a[i]);
+                        break;
+                    } else if ((int) stack.peek() > a[i]) {
+                        int max = (int)stack.peek();
+                        stack.push(a[i]);
+                        int j = i+1;
+                        while(max>a[j] && j<num){
+                            ++j;
+                            if(j>num && max>a[j]){
+                                stack.pop();
+                                System.out.println(-1);
+                                break;
+                            }
+
+                        }
+                        break;
+                    }
+                    }
                 }
             }
-            if((int)stack.peek()>a[i]){
-                stack.push(a[i]);
-
+            if (stack.empty() != true) {
+                System.out.println((int) stack.pop());
+                if (stack.empty() != true) {
+                    System.out.println(-1);
+                    stack.pop();
+                }
             }
         }
-        for(int x:result){
-            System.out.println(x);
-        }
     }
-}
-
-
-
-
-
-
-
-
