a2149e4ba84c095df4bd7e5df3d8673cae0c3c28
bracksnow <ktom97@gmail.com>
2019-11-10 05:20:15 +0900

    no message

---
+++ 마이리얼트립/javascript/slideshow.js
@@ -0,0 +1,18 @@
+
+    $(".indicator ul li").click(function(){
+        $(".indicator ul li").removeClass("active");
+        $(this).addClass("active");
+        var i = $(this).index()+1;
+        $(".slideshow img").removeClass("active");
+        $(".slideshow").find("img:nth-child("+i+")").addClass("active");
+        });
+        $(function($){
+            var lis = $(".slideshow>img");
+            var menu  = $(".indicator ul>li");
+            ch = 0;
+            setInterval(function() {
+            ch = (ch + 1) % lis.length;
+            lis.removeClass("active").eq(ch).addClass("active");
+            menu.removeClass("active").eq(ch).addClass("active");
+        },3000);
+});
