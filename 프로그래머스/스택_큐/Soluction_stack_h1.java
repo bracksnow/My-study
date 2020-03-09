6eaa3bdfa536cd057b0d27319bc42178eb7e5669
bracksnow <ktom97@gmail.com>
2020-03-10 02:09:33 +0900

    no message

---
+++ 프로그래머스/스택_큐/Soluction_stack_h1.java
@@ -0,0 +1,22 @@
+package DataStructure;
+
+public class Soluction_stack_h1 {
+    public int[] solution(int[] heights) {
+        int len = heights.length;
+        int[] answer = new int[len];
+        for(int i=0 ; i<len ; i++){
+            boolean flag = false;
+            for(int j=i-1 ; j>=0 ; j--){
+                if(heights[j]>heights[i]){
+                    flag = true;
+                    answer[i] = j+1;
+                    break;
+                }
+            }
+            if(!flag){
+                answer[i] = 0;
+            }
+        }
+        return answer;
+    }
+}
