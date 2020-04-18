83d2bef3dc8b92fc8bba25772fea583f02dba108
bracksnow <ktom97@gmail.com>
2020-04-18 18:12:05 +0900

    no message

--- 프로그래머스/정렬/Solution_sort_h3.py
+++ 프로그래머스/정렬/Solution_sort_h3.py
@@ -1,7 +1,7 @@
 def solution(citations):
     answer = 0
     citations.sort()
-    for i in range(0,len(citations)):#0부터 len(citations)-1이 범위임
+    for i in range(0,len(citations)):#0부터 len(citations)-1이 범위
         h = len(citations)-i
         if citations[i]>=h:
             answer = h
