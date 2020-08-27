03e9731f33596a8d86a4b1238ad0935572f49186
bracksnow <ktom97@gmail.com>
2020-08-28 01:58:19 +0900

    no message

--- 자바 자료구조(백준)/Boj1013.java
+++ 자바 자료구조(백준)/Boj1013.java
@@ -12,9 +12,10 @@ public class Boj1013 {
         int T = Integer.parseInt(bf.readLine());//테스트 횟수
         while(T-->0){
             String reg = bf.readLine();
-            Pattern pattern = Pattern.compile("((100+1+)|(01))+");
-            Matcher matcher = pattern.matcher(reg);
-            System.out.println(matcher.matches()? "YES":"NO");
+            Pattern pattern = Pattern.compile("((100+1+)|(01))+");//정규표현식을 만듦
+            Matcher matcher = pattern.matcher(reg);//pattern 과 reg로 패턴확인
+            System.out.println(matcher.matches()? "YES":"NO");//패턴일 경우에는 true 아니면 false
+            //삼항연산자 이용함
         }
     }
 }
