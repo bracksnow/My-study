1099020179d9fa441653a1d5d9a6b72ff4ad534a
bracksnow <ktom97@gmail.com>
2020-12-12 23:27:58 +0900

    no message

--- 웹 연습/spring/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 웹 연습/spring/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -19,7 +19,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item; //주문 상품-다대일의 관계
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)//Xtoone인 것들은 기본이 EAGER이다. EAGER은 SQL문을 날릴때 한꺼번에 로딩을 하기 때문에 N+1문제가 발생할 가능성이 높다
     @JoinColumn(name = "order_id")
     private Order order; //주문-다대일의 관계
 
