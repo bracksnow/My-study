88da2f9a531ff69e6298ed5d3722dd6ef36f4784
bracksnow <ktom97@gmail.com>
2020-12-13 23:29:08 +0900

    no message

---
+++ 웹 연습/spring/jpashop/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)//성능이 좀 더 좋아진다
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional //이런 식으로 설정하면 기본은 readonly이지만 join에서만 다르게 걸린다
+    public Long join(Member member){
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers =
+                memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+
+    }
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
