ea58a6acab1960d967672e9dae8a5342176d4f35
bracksnow <ktom97@gmail.com>
2020-12-21 15:50:49 +0900

    no message

--- 웹 연습/spring/core/core/src/main/java/hello/core/MemberApp.java
+++ 웹 연습/spring/core/core/src/main/java/hello/core/MemberApp.java
@@ -5,11 +5,12 @@ import hello.core.Member.MemberService;
 import hello.core.Member.MemberServiceImpl;
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
-        System.out.println("find hello.core.Member = " + findMember.getName());
+        System.out.println("find Member = " + findMember.getName());
     }
 }
