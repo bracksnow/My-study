030678805efe471272154846188ca2f24f27fd4f
bracksnow <ktom97@gmail.com>
2020-12-31 01:32:26 +0900

    no message

---
+++ 프로그래머스/level2/MakeMinimum.java
@@ -0,0 +1,15 @@
+package DataStructure;
+
+import java.util.Arrays;
+
+public class MakeMinimum {
+    public int solution(int []A, int []B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i = 0;i<A.length;i++){
+            answer+=A[i]*B[A.length-i-1];
+        }
+        return answer;
+    }
+}
