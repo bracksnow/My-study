ad86949f337071ec59033befd64bf603b4a6c736
bracksnow <ktom97@gmail.com>
2021-01-09 23:59:06 +0900

    no message

--- 블로그 만들기/jpablog/jpablog/src/main/java/blog/jpablog/controller/PostController.java
+++ 블로그 만들기/jpablog/jpablog/src/main/java/blog/jpablog/controller/PostController.java
@@ -5,12 +5,19 @@ import blog.jpablog.service.MemberService;
 import blog.jpablog.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;//생성자 주입
 
+    @GetMapping("/list")
+    public String list(){
+        return "board/list";
+    }
 
 }
