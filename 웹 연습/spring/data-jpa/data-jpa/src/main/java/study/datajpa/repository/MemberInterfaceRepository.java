9b2fd997c77c3eaef8895b9c94ae7b547fc2b93b
bracksnow <ktom97@gmail.com>
2021-01-11 15:48:32 +0900

    no message

--- 웹 연습/spring/data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberInterfaceRepository.java
+++ 웹 연습/spring/data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberInterfaceRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.domain.Member;
+import study.datajpa.dto.MemberDto;
 
 import java.util.List;
 
@@ -14,5 +15,32 @@ public interface MemberInterfaceRepository extends JpaRepository<Member, Long> {
     @Query("select m from Member m where m.username=:username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
     //위의 것이 실무에서 가장 많이 사용하는 내용임 ->오타를 적는 경우엔 로딩시점에 오류가 발생
-    
+
+
+    @Query("select m.username from Member m")
+    List<String> findUserName();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String>names);
+
+    //단건 조회에서 데이터가 있는지 없는지 확신이 없는 경우엔 optional로 감싸주기
+    /*
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by
+                m.username desc")
+                        .setParameter("age", age)
+                        .setFirstResult(offset)
+                        .setMaxResults(limit)
+                        .getResultList();
+    }
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age",
+                Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
+    */
 }
