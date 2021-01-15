1612d532780ed8c4ded78067ba9fb4dac5b98010
bracksnow <ktom97@gmail.com>
2021-01-15 21:48:14 +0900

    no message

--- 블로그 만들기/blog/blog/src/main/java/simpleblog/blog/controller/BoardController.java
+++ 블로그 만들기/blog/blog/src/main/java/simpleblog/blog/controller/BoardController.java
@@ -4,11 +4,14 @@ package simpleblog.blog.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import simpleblog.blog.domain.Board;
 import simpleblog.blog.repository.BoardRepository;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
@@ -24,15 +27,24 @@ public class BoardController {
         return "board/list";
     }
 
-    @GetMapping
-    public String form(){
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id==null){
+            model.addAttribute("board", new Board());//->에러가 생긴 이유는 board와 폼을 비교해야 했기 때문
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
         return "board/form";
     }
 
     @PostMapping("/form")
-    public String boardSubmit(@ModelAttribute Board board){
+    public String boardSubmit(@Valid Board board, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {//에러가 있는 경우
+            return "board/form";
+        }
         boardRepository.save(board);
-        return "redirect:board/list";
+        return "redirect:/board/list";
 
     }
 }
--- 블로그 만들기/blog/blog/src/main/java/simpleblog/blog/domain/Board.java
+++ 블로그 만들기/blog/blog/src/main/java/simpleblog/blog/domain/Board.java
@@ -1,23 +1,34 @@
 package simpleblog.blog.domain;
 
+import com.sun.istack.NotNull;
 import lombok.Data;
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
-public class Board {
+public class Board extends DateEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
+    @NotBlank(message = "제목을 입력하세요")
     private String title;
 
+    @NotBlank(message = "내용을 입력하세요")
     private String content;
+
+
+
+
+    
+
+
+
 
 
 
