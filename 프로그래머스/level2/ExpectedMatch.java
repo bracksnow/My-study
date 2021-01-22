72d5fb33a8d9a0af6f6db92f54b57de9cb3e0094
bracksnow <ktom97@gmail.com>
2021-01-22 15:44:35 +0900

    Create ExpectedMatch.java
    예상대진표

---
+++ 프로그래머스/level2/ExpectedMatch.java
@@ -0,0 +1,13 @@
+package DataStructure;
+
+public class ExpectedMatch {
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+        while(a != b){
+            answer ++;
+            a = (a+1)/2;
+            b = (b+1)/2;
+        }
+        return answer;
+    }
+}
