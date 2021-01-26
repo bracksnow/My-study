01fee0139f97d6fdb2172ab49ef1d43718c2d570
bracksnow <ktom97@gmail.com>
2021-01-26 15:34:09 +0900

    no message

--- 프로그래머스/level2/SkillTree.java
+++ 프로그래머스/level2/SkillTree.java
@@ -9,30 +9,12 @@ public class SkillTree {
         System.out.println(solution(skill, skill_trees));
     }
     public static int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        Queue<String> queue = new LinkedList<String>();
-        for(int i = 0;i<skill.length();i++){
-            queue.add(skill.substring(i, i+1));
-        }//큐에 skill문자열을 잘라서 넣음
-        int fanswer = 1;
-        while(!queue.isEmpty()){
-            int count = 0;
-            String startAlphabet = queue.poll();//앞에 있는 것부터 나와야하기 때문에 큐로 자료구조 선정
-            for(int i = 0;i<skill_trees.length;i++){
-                String compareAlphabet = skill_trees[i].substring(0,1);
-                if(startAlphabet.equals(compareAlphabet)) {
-                    count++;
-                }
-                else{
-                    count = 1;
-                }
+        int answer = skill_trees.length;
+        for(int i = 0;i< skill_trees.length;i++){
+            if(skill.indexOf(skill_trees[i].replaceAll("[^"+skill+"]",""))!=0){
+                answer--;
             }
-            fanswer*=count;
         }
-        if(fanswer==1){
-            return answer;
-        }else{
-            return fanswer;
-        }
+        return answer;
     }
 }
